39f201918efc35f9001e9369210d9e36fd9bccfa
Noonexyz1 <119150599+Noonexyz1@users.noreply.github.com>
2024-04-17 00:53:14 -0400

    Add files via upload

---
+++ src/main/resources/static/web-law/src/App.jsx
@@ -0,0 +1,17 @@
+
+import { FooterWithSocialLinks } from "./components/ui/FooterWithSocialLinks"
+import { GalleryWithCarousel } from "./components/ui/GalleryWithCarousel"
+import Header from "./components/ui/Header"
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <GalleryWithCarousel/>
+      <FooterWithSocialLinks/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/test/java/com/dametumanolegal/DametumanolegalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dametumanolegal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DametumanolegalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
