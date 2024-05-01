9a157e81a38f1367daddcb9ca39d2b1ab6a3d09e
Noonexyz1 <aries-d2github@hotmail.com>
2024-05-01 14:34:45 -0400

    Frontend buildeado y puesto a static en el backend

--- frontend-law/src/components/ui/Dashboard.jsx
+++ frontend-law/src/components/ui/Dashboard.jsx
@@ -379,7 +379,7 @@ export default function Dashboard() {
                                 <div className="flex flex-row justify-around flex-wrap py-5 px-6 text-white">
                                     {/*Para mis tarjetas etiquetas */}
                                     {stats.map((stat) => (
-                                        <div key={stat.id} className="flex flex-row justify-start border border-white rounded-2xl w-48 h-24 my-2 mx-2">
+                                        <div key={stat.id} className="flex flex-row justify-start backdrop-blur-3xl border border-white rounded-2xl w-48 h-24 my-2 mx-2">
                                             <div className='px-2 py-2'>
                                                 <img src={stat.icono} alt="icono-card" />
                                             </div>
