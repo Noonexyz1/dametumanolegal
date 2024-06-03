52f3ea0da44a31570d17a2f7ff73f41437829f94
Noonexyz1 <aries-d2github@hotmail.com>
2024-06-02 20:16:38 -0400

    backend-law tests unitarios para StaffLegalTest iniciado

---
+++ backend-law/src/test/java/com/dametumanolegal/domain/modStaffLegal/StaffLegalTest.java
@@ -0,0 +1,17 @@
+package com.dametumanolegal.domain.modStaffLegal;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StaffLegalTest {
+    @Test
+    public void iniciarSesion() {
+        Assertions.assertTrue(true);
+    }
+    public void cerrarSesion() {
+
+    }
+    public void modificarPassword() {
+
+    }
+}
