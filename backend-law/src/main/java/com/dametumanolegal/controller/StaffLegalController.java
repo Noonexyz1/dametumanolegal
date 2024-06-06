0c6c3e9c7f9b2e7f70c57c82a4598cef5e6b1b9f
Noonexyz1 <aries-d2github@hotmail.com>
2024-06-06 11:57:30 -0400

    integracion backend, frontend del modulo StaffLegal iniciado

--- backend-law/src/main/java/com/dametumanolegal/controller/StaffLegalController.java
+++ backend-law/src/main/java/com/dametumanolegal/controller/StaffLegalController.java
@@ -4,10 +4,12 @@ import com.dametumanolegal.domain.port.Autenticable;
 import com.dametumanolegal.request.InitSesionRequest;
 import com.dametumanolegal.response.InitSesionResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class StaffLegalController implements Autenticable {
     @Autowired
